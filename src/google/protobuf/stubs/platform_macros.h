549254e4fa1089cdfab3be69ac5fe1b0e8266600
Daniel Treiman <dtreiman@alivecor.com>
2019-01-31 23:09:43 -0800

    Testing arm64.

--- src/google/protobuf/stubs/platform_macros.h
+++ src/google/protobuf/stubs/platform_macros.h
@@ -50,7 +50,7 @@
 #elif defined(__ARMEL__)
 #define GOOGLE_PROTOBUF_ARCH_ARM 1
 #define GOOGLE_PROTOBUF_ARCH_32_BIT 1
-#elif defined(__aarch64__)
+#elif defined(__aarch64__) || defined(__arm64__)
 #define GOOGLE_PROTOBUF_ARCH_AARCH64 1
 #define GOOGLE_PROTOBUF_ARCH_64_BIT 1
 #elif defined(__MIPSEL__)
